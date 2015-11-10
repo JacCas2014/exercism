9a0752fa3821daeae86aa218f288d6cc92bedfce
Jac Casey <JacCas2014@users.noreply.github.com>
2015-11-10 22:23:35 +0000

    Jac - un-future facting tests that are now passing

--- bob_test.clj
+++ bob_test.clj
@@ -12,25 +12,25 @@
 (fact "responds-to-questions"
       (response-for "Does this cryogenic chamber make me look fat?") => "Sure.")
 
-(future-fact "responds-to-forceful-talking"
+(fact "responds-to-forceful-talking"
       (response-for "Let's go make out behind the gym!") => "Whatever.")
 
-(future-fact "responds-to-acronyms"
+(fact "responds-to-acronyms"
       (response-for "It's OK if you don't want to go to the DMV.") => "Whatever.")
 
-(future-fact "responds-to-forceful-questions"
+(fact "responds-to-forceful-questions"
       (response-for "WHAT THE HELL WERE YOU THINKING?") => "Whoa, chill out!")
 
-(future-fact "responds-to-shouting-with-special-characters"
+(fact "responds-to-shouting-with-special-characters"
       (response-for "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!") => "Whoa, chill out!")
 
-(future-fact "responds-to-shouting-numbers"
+(fact "responds-to-shouting-numbers"
       (response-for "1, 2, 3 GO!") => "Whoa, chill out!")
 
-(future-fact "responds-to-shouting-with-no-exclamation-mark"
+(fact "responds-to-shouting-with-no-exclamation-mark"
       (response-for "I HATE YOU") => "Whoa, chill out!")
 
-(future-fact "responds-to-statement-containing-question-mark"
+(fact "responds-to-statement-containing-question-mark"
       (response-for "Ending with ? means a question.") => "Whatever.")
 
 (future-fact "responds-to-silence"
